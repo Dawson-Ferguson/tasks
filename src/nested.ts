b7987a88653fa326b260fcdddf461113608e7fe5
Dawson Ferguson <sunnydwf9@gmail.com>
2026-02-22 16:46:37 -0500

    addNewQuestion test passed

--- src/nested.ts
+++ src/nested.ts
@@ -113,7 +113,22 @@ export function addNewQuestion(
     name: string,
     type: QuestionType,
 ): Question[] {
-    return [];
+    const aQuestion = {
+        id: id,
+        name: name,
+        type: type,
+        body: "",
+        expected: "",
+        options: [],
+        points: 1,
+        published: false,
+    };
+    const questionsCopy = questions.map(
+        (question: Question): Question => ({
+            ...question,
+        }),
+    );
+    return [...questionsCopy, aQuestion];
 }
 
 /***
